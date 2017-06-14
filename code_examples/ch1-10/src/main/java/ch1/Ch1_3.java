36aa4df5fde7a4147933f9628a36bf17c8af26c9
Thomas Nield <thomasnield@live.com>
2017-06-13 19:27:43 -0500

    redo

--- code_examples/ch1-10/src/main/java/ch1/Ch1_3.java
+++ code_examples/ch1-10/src/main/java/ch1/Ch1_3.java
@@ -2,14 +2,22 @@ package ch1;
 
 import io.reactivex.Observable;
 
+import java.util.concurrent.TimeUnit;
+
 public class Ch1_3 {
- public static void main(String[] args) {
-  
-   Observable<String> myStrings =
-     Observable.just("Alpha", "Beta", "Gamma", "Delta",
-      "Epsilon");
-  
-     myStrings.map(s -> s.length()).subscribe(s -> 
-     System.out.println(s));
-  }
+    public static void main(String[] args) {
+        Observable<Long> secondIntervals =
+                Observable.interval(1, TimeUnit.SECONDS);
+        secondIntervals.subscribe(s -> System.out.println(s));
+/* Hold main thread for 5 secondsso Observable above has chance to fire */
+        sleep(5000);
+    }
+
+    public static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
